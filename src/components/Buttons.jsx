07f2f52ef708580cee86abf4b609b13b153bb07b
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-25 18:18:23 +0545

    Revert: Display rocket info from dummy data in slice

--- src/components/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/Rockets.css';
-
-const Buttons = () => (
-  <div>
-    <button type="button" className="btn-reserve">Reserve Rocket</button>
-  </div>
-);
-
-export default Buttons;
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,29 +1,7 @@
-import { useSelector } from 'react-redux';
-import Buttons from './Buttons';
-import '../styles/Rockets.css';
-
-const Rockets = () => {
-  const rocketsList = useSelector((state) => state.rockets);
-
-  return (
-    <div className="rockets-container">
-      {rocketsList.map((item) => (
-        <section key={item.id} className="rocket-list">
-          <div
-            className="rocket-item"
-            key={item.id}
-          >
-            <img src={item.flickr_images[0]} alt="rocket" />
-            <div className="rocket-info">
-              <h3 className="rocket-name">{item.name}</h3>
-              <p>{item.description}</p>
-              <Buttons />
-            </div>
-          </div>
-        </section>
-      ))}
-    </div>
-  );
-};
+const Rockets = () => (
+  <div>
+    <h2>Rockets</h2>
+  </div>
+);
 
 export default Rockets;
