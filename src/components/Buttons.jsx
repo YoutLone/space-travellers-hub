f7fd4beb7c7a9a058d08ccaf91f2b2f1e581866f
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-25 18:11:02 +0545

    Display rocket info from dummy data in slice

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,9 @@
+import '../styles/Rockets.css';
+
+const Buttons = () => (
+  <div>
+    <button type="button" className="btn-reserve">Reserve Rocket</button>
+  </div>
+);
+
+export default Buttons;
--- src/redux/Rockets/RocketsSlice.js
+++ src/redux/Rockets/RocketsSlice.js
@@ -5,18 +5,21 @@ const initialState = [
     id: 'item1',
     name: 'Falcon 1',
     type: 'rocket',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     flickr_images: ['https://imgur.com/DaCfMsj.jpg', 'https://imgur.com/azYafd8.jpg'],
   },
   {
     id: 'item2',
     name: 'Falcon 2',
     type: 'rocket',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     flickr_images: ['https://imgur.com/DaCfMsj.jpg', 'https://imgur.com/azYafd8.jpg'],
   },
   {
     id: 'item3',
     name: 'Falcon 3',
     type: 'rocket',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     flickr_images: ['https://imgur.com/DaCfMsj.jpg', 'https://imgur.com/azYafd8.jpg'],
   },
 ];
