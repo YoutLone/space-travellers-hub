6a5c2a949313f7f5bbb6d30a9d3a0612a0f3e4bd
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-26 14:17:30 +0545

    Display reserved rockets in My Profile

--- src/components/RocketProfile.jsx
+++ src/components/RocketProfile.jsx
@@ -1,7 +1,20 @@
-const RocketProfile = () => (
-  <div>
-    <p>My Rockets list here</p>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const RocketProfile = () => {
+  const rocketsList = useSelector((state) => state.rockets.Rockets);
+  const reservedRocketsList = rocketsList.filter((item) => item.reserved);
+
+  return (
+    <section>
+      <ul>
+        {reservedRocketsList.map((item) => (
+          <li key={item.id}>
+            <p>{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default RocketProfile;
